36a4b8b8b3c157fdbc601a691483a4b2e74cfa26
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-08-25 10:54:30 +1200

    Add project files.

---
+++ Controllers/ReactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_Web_API_Demo.Controllers
+{
+    public class ReactController : Controller
+    {
+        // GET: React
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Scripts/Home/react/index.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+const App = () => (
+    <React.StrictMode>
+        <Router>
+            <div> React Routing Test </div>
+            <div><Link to="/"> Index </Link></div>
+            <div><Link to="/test"> Test </Link></div>
+            <div> <Link to="/test2"> Test2 </Link> </div>
+            <Switch>
+                <Route path="/test">
+                    <div>Test</div>
+                </Route>
+                <Route path="/test2">
+                    <div>Test2</div>
+                </Route>
+            </Switch>
+        </Router>
+    </React.StrictMode>
+);
+
+render(<App />, document.getElementById('app'));
