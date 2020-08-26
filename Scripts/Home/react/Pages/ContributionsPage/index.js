6d9c4186d105604ec4836da64c729a653309da79
Tyger Kong <tyger.kong@hotmail.com>
2020-08-27 11:56:36 +1200

    Add contributions page structure

---
+++ Scripts/Home/react/Pages/ContributionsPage/index.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import ContributionsContainer from '../../Container/ContributionsContainer';
+import ContributionsPage from './ContributionsPage'
+
+export default () => {
+    return (
+        <ContributionsContainer>
+            <ContributionsPage />
+        </ContributionsContainer>
+    );
+};
