8e4a9436547e68b5959a8b7e3c0a05ac7e3ac15f
Ryan Lim <ryanlimenwei@gmail.com>
2020-08-27 16:14:34 +1200

    Add placeholder forms for contributions page

--- Scripts/Home/react/Pages/ContributionsPage/ContributionsPage.js
+++ Scripts/Home/react/Pages/ContributionsPage/ContributionsPage.js
@@ -1,10 +1,24 @@
 ﻿import React from 'react';
+import { Row, Col, Container, Form, FormGroup, Label, Input} from 'reactstrap';
 
 const ContributionsPage = ({ onClick }) => {
     return (
         <>
             <button onClick={onClick} type="button"> Click </button>
             <div>Contributions</div>
+
+            <Container>
+                <Form>
+                    <Row>
+                        <Col>
+                            <FormGroup>
+                                <Label for="exampleEmail">Email</Label>
+                                <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                </Form>
+            </Container>
         </>
 
         );
