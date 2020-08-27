a6aa273921303602a9b6643ef8733e6444227121
Tyger Kong <tyger.kong@hotmail.com>
2020-08-27 16:31:11 +1200

    Add  handleChange and Blur functions

--- Scripts/Home/react/Container/ContributionPageContainer.js
+++ Scripts/Home/react/Container/ContributionPageContainer.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContributionPageContainer = ({ children }) => {
   // Any variables or methods declared in newProps will be passed through to children
   // components as declared in frontpage.jsx
 
-  const handleChange = () => {
-    console.log("handleChange");
-  };
+    const [details, setDetails] = useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+    })
 
-  const newProps = { handleChange };
 
-  return React.cloneElement(children, { ...newProps });
+    const handleChange = (event) => {
+        const newDetails = JSON.parse(JSON.stringify(details));
+        const { name, value } = event.target;
+
+        newDetails[name] = value;
+
+        setDetails(newDetails);
+    }
+
+    const onBlur = (event) => {
+        console.log(details);
+    }
+
+    const newProps = { details, handleChange, onBlur };
+
+    return React.cloneElement(children, { ...newProps });
 };
 
 export default ContributionPageContainer;
