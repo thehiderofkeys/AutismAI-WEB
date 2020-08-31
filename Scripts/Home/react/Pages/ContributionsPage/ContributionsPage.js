f0076eb33baa6f238b8b0cf00eaa3291a8ce7268
Tyger Kong <tyger.kong@hotmail.com>
2020-08-31 19:47:44 +1200

    Add description back

--- Scripts/Home/react/Pages/ContributionsPage/ContributionsPage.js
+++ Scripts/Home/react/Pages/ContributionsPage/ContributionsPage.js
@@ -1,21 +1,31 @@
 ﻿import React from 'react';
 import { Row, Col, Container, Form, FormGroup, Label, Input, Button, InputGroup, InputGroupAddon, InputGroupText} from 'reactstrap';
 import { User, Mail, Edit } from 'react-feather';
-import { EmailContainer, TextContent } from './ContributionPageStyles';
+import { EmailContainer, TextContent, EmailTitlesContainer } from './ContributionPageStyles';
+import { HeaderText, SubText } from "../Frontpage/frontpageStyles";
 
 const ContributionsPage = ({ details, handleChange, onBlur, onSubmit, disableSubmit }) => {
     return (
         <>
             <EmailContainer>
-                <Form action="https://forms.gle/beuGqT3rm6j6ndsj7" target="_blank">
-                    <h2>About the team</h2>
-                    <TextContent>
-                        Lorem Ipsum
-                    </TextContent>      
-                    <h2>Contributors</h2>
-                    <TextContent>
-                        Lorem Ipsum 
-                    </TextContent>      
+                <Form action="https://forms.gle/beuGqT3rm6j6ndsj7" target="_blank"> 
+                    <EmailTitlesContainer>
+                        <HeaderText>About the team</HeaderText>
+                        <TextContent>
+                            Lorem Ipsum
+                    </TextContent>
+                        <HeaderText>Contributors</HeaderText>
+                        <TextContent>
+                            Lorem Ipsum
+                    </TextContent>  
+                        <HeaderText>
+                            Looking to help out?
+                             </HeaderText>
+                        <SubText className="d-none d-md-flex">
+                            Please feel free to send us a message regarding anything you'd like to contribute (art, data, opinions, etc).
+                             </SubText>
+                    </EmailTitlesContainer>
+  
                     <Button color="primary" type="submit">Contact Us</Button>
                 </Form>
             </EmailContainer>
