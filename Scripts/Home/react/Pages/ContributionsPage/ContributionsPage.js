6909482502144b3ca19b67828c93a8099132ae31
Ryan Lim <ryanlimenwei@gmail.com>
2020-09-02 10:54:57 +1200

    Update contribution write up for public interaction

--- Scripts/Home/react/Pages/ContributionsPage/ContributionsPage.js
+++ Scripts/Home/react/Pages/ContributionsPage/ContributionsPage.js
@@ -20,9 +20,14 @@ const ContributionsPage = ({
                 <Form action="https://forms.gle/beuGqT3rm6j6ndsj7" target="_blank">
                     <EmailTitlesContainer>
                         <HeaderText>Looking to help out?</HeaderText>
-                        <SubText className="d-none d-md-flex">
-                            Please feel free to send us a message regarding anything you'd like to
-                            contribute (art, data, opinions, etc).
+                        <SubText className="d-flex flex-column align-items-start">
+                            <p>Hi everyone!</p>
+                            <p>Thank you for taking the time to provide us with your valuable feedback.</p>
+                            <p>The team requires some graphics from you, namely:</p>
+                            <p><i>- A new background image (That will replace the current bridge photo)</i></p>
+                            <p><i>- A brand new Autism AI logo</i></p>
+                            <p>Draw up your designs and send them in to the google form.</p>
+                            <p>Click the "Contact Us" button below to send in your graphics and feedback.</p>
                         </SubText>
                         <Button color="primary" type="submit">
                             Contact Us
