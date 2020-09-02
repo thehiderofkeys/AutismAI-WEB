817163fc224a876ad2951504c84de0130202cb8f
SamNeale <snea076@aucklanduni.ac.nz>
2020-09-02 19:25:21 +1200

    Styled question component inline with wireframe mockups

--- Scripts/Home/react/Components/Question/Question.js
+++ Scripts/Home/react/Components/Question/Question.js
@@ -4,20 +4,25 @@ import { Row, Col, Container, FormGroup, Label, legend, Input } from "reactstrap
 const Question = ({ question, answerOptions }) => {
     console.log(question);
     return (
+
         <Container>
-            <Row>
-                <Col className="questionText"></Col>
-            </Row>
-            <FormGroup tag="fieldset">
-                <legend>{question.questionText}</legend>
-                {answerOptions.map((option, i) => (
-                    <FormGroup check key={`${question.name}-${i}-key`}>
-                        <Label check>
-                            <Input type="radio" name={`${question.name}-option`} /> {option}
-                        </Label>
+            <Row className="align-items-center">
+                <Col className="d-flex justify-content-center">
+                    <FormGroup tag="fieldset">
+                        <legend>{question.questionText}</legend>
+                        {answerOptions.map((option, i) => (
+                            <FormGroup check key={`${question.name}-${i}-key`}>
+                                <Row className="mt-3 border rounded mw-100 ">
+                                    <Label check className="ml-4">
+                                        <Input type="radio"  name={`${question.name}-option`} /> {option}
+                                    </Label>
+                                </Row>
+                            </FormGroup>
+                        ))}
                     </FormGroup>
-                ))}
-            </FormGroup>
+                </Col>
+            </Row>
+            
         </Container>
     );
 };
--- Scripts/Home/react/Pages/QuizPage/QuizPage.js
+++ Scripts/Home/react/Pages/QuizPage/QuizPage.js
@@ -6,8 +6,6 @@ import Question from "../../Components/Question/Question";
 const Quizpage = ({ handleChange, question, answerOptions }) => {
     return (
         <div className={styles["test"]}>
-            <p>This is the Quiz 2 Page.</p>
-
             <Question question={question} answerOptions={answerOptions} />
         </div>
     );
