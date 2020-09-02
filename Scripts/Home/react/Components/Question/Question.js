ec5302a9bc944183fa39f6e3a4f9384a5623ea8b
SamNeale <snea076@aucklanduni.ac.nz>
2020-09-02 12:10:19 +1200

    Created basic component structure for question text and radio button answer display

---
+++ Scripts/Home/react/Components/Question/Question.js
@@ -0,0 +1,36 @@
+﻿import React from "react";
+import { Row, Col, Container, FormGroup, Label, legend, Input } from "reactstrap";
+
+const Question = ({ questionText, answerOptions }) => {
+
+    const radioItems = []
+
+    for (const [index, value] of answerOptions.entries()) {
+        let name = "radio" + index 
+        radioItems.push(
+            <Container>
+                <FormGroup check>
+                    <Label check>
+                        <Input type="radio" name={name} />{' '}
+                        {value}
+                    </Label>
+                </FormGroup>
+            </Container>
+            )
+    }
+
+    return (
+        <Question>
+            <Container>
+                <Row>
+                    <Col className="questionText">
+                    </Col>
+                </Row>
+                <FormGroup tag="fieldset">
+                    <legend>{questionText}</legend>
+                    {radioItems}
+                </FormGroup>
+            </Container>
+        </Question>
+);
+}
