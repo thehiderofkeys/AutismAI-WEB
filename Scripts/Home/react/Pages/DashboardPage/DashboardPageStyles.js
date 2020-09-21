5f1e18684bc19adec750ef048c034d4ef7e3f2c9
Mario Sinovcic <38930019+MarioSinovcic@users.noreply.github.com>
2020-09-21 16:06:46 +1200

    Basic Components Added to the Dashboard Page

---
+++ Scripts/Home/react/Pages/DashboardPage/DashboardPageStyles.js
@@ -0,0 +1,22 @@
+﻿import styled from 'styled-components';
+
+export const DashboardContainer = styled.div`
+  width: 100%;
+  margin-top: 150px;
+  margin-bottom: 200px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+export const SubTitle = styled.div`
+    font-size: 22px;
+    margin-bottom: 1em;
+`;
+
+export const GraphTitle = styled.div`
+    font-size: 20px;
+    margin-bottom: 1em;
+`;
