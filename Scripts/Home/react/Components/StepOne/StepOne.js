8b10be87d688f33698f18d3eea1a97c7e6b3a8ba
SamNeale <snea076@aucklanduni.ac.nz>
2020-09-23 11:52:25 +1200

    Fixed centering issue on stepOne component

--- Scripts/Home/react/Components/StepOne/StepOne.js
+++ Scripts/Home/react/Components/StepOne/StepOne.js
@@ -9,39 +9,24 @@ const StepOne = ({handleChange, stepOneAnswers, ethnicities}) => {
     console.log(ethnicities)
     return (
         <Container className="d-flex justify-content-center">
-            <Col>
-                <Row className="d-flex justify-content-center">
-                    
-
+            <Col style={{ maxWidth:'30%' }}>
                     <FormGroup tag="fieldset">
                         <legend>Gender</legend>
                         <ButtonGroup >
                             <Button className="bg-white text-dark" style={{ border: '1px solid #ced4da' }}>Male</Button>
                             <Button className=" bg-white text-dark" style={{ border: '1px solid #ced4da' }}>Female</Button>
                         </ButtonGroup>
-                    </FormGroup>
-                </Row>
 
-                <Row className="d-flex justify-content-center">
-                    <FormGroup tag="fieldset">
                         <legend>Ethnicity</legend>
                         <Input type="select" name="Ethnicity" id="exampleSelect" className="mw-75">
                             {ethnicities.map((option) => (
                                 <option>{option}</option>
                             ))}
                         </Input>
+                            <legend>Age</legend>
+                            <Input type="textbox" name="text" id="exampleText" className="mw-75" />
                     </FormGroup>
-                </Row>
-
-                <Row className="d-flex justify-content-center">
-                    <FormGroup>
-                        <legend>Age</legend>
-                        <Input type="textbox" name="text" id="exampleText" className="mw-75" />
-                    </FormGroup>
-
-                </Row>
             </Col>
-            
         </Container>
     );
 };
