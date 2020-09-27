4fe440fe2da73aff0ec122256b65b472e96dbbfe
Tyger Kong <tyger.kong@hotmail.com>
2020-09-27 20:24:14 +1300

    Add toddler answerSet questions

--- Scripts/Home/react/services/questionsService.js
+++ Scripts/Home/react/services/questionsService.js
@@ -7,7 +7,7 @@ export const categories = {
     CHAT: "questions_chat"
 };
 
-const answerSet = {
+const answerSetType = {
     AGREE: "agree_answer_options",
     FREQUENCY: "frequency_answer_options",
     EASE: "ease_answer_options",
@@ -15,36 +15,62 @@ const answerSet = {
     FIRST_WORD: "first_word_answer_options"
 };
 
-export const getAnswerSet = (answerSetType) => {
-    return questions[answerSetType];
+export const getAnswerSet = (answerSet) => {
+    return questions[answerSet];
 };
 export const getQuestions = ({ age, unit }) => {
     let category = categories.ADULT;
+    let calculatedAge;
+    let isToddler = false;
+
     if (unit === "Years") {
-        switch (true) {
-            case age <= 11:
-                console.log("child");
-                category = categories.CHILD;
-                break;
-            case age <= 16:
-                category = categories.ADOLESCENT;
-                break;
-            default:
-                category = categories.ADULT;
-                break;
-        }
+        calculatedAge = age;
+    } else {
+        calculatedAge = age / 12;
+    }
+
+    switch (true) {
+        case calculatedAge < 3:
+            category = categories.CHAT;
+            isToddler = true;
+        case calculatedAge <= 11:
+            console.log("child");
+            category = categories.CHILD;
+            break;
+        case calculatedAge <= 16:
+            category = categories.ADOLESCENT;
+            break;
+        default:
+            category = categories.ADULT;
+            break;
     }
 
     const retrievedQuestions = questions[category];
+
     const mappedQuestions = retrievedQuestions.map((question, i) => {
+        let answerSet = getAnswerSet(answerSetType.AGREE);
+        if (isToddler) {
+            switch (true) {
+                case i === 0 || i === 6:
+                    answerSet = getAnswerSet(answerSetType.FREQUENCY);
+                    break;
+                case i === 1:
+                    answerSet = getAnswerSet(answerSetType.EASE);
+                    break;
+                case i === 7:
+                    answerSet = getAnswerSet(answerSetType.FIRST_WORD);
+                    break;
+                default:
+                    answerSet = getAnswerSet(answerSetType.DAILY_FREQ);
+            }
+        }
         return {
             name: `Question ${i + 1}`,
             questionText: question,
-            answerSet: getAnswerSet(answerSet.EASE)
+            answerSet
         };
     });
     console.log(mappedQuestions);
-
     return mappedQuestions;
 };
 
