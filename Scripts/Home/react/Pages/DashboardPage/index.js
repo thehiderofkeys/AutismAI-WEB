11954265fc137638b270021b435cef280cd9c9c6
Ryan Lim <ryanlimenwei@gmail.com>
2020-09-27 15:49:56 +1300

    Fix overwritten files

---
+++ Scripts/Home/react/Pages/DashboardPage/index.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import DashboardPageContainer from '../../Container/DashboardPageContainer';
+import DashboardPage from './DashboardPage'
+
+export default () => {
+    return (
+        <DashboardPageContainer>
+            <DashboardPage />
+        </DashboardPageContainer>
+    );
+};
