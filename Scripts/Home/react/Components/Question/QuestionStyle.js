d17276a81ca3f747a3e0177f1ce05b5b6b23e70b
SamNeale <snea076@aucklanduni.ac.nz>
2020-09-30 14:34:46 +1300

    Made height consistent for question component

--- Scripts/Home/react/Components/Question/QuestionStyle.js
+++ Scripts/Home/react/Components/Question/QuestionStyle.js
@@ -2,9 +2,11 @@
 
 export const QuizContainer = styled.div`
   width: 65%;
-  height: 65%;
+  height: 410px;
+  overflow: auto;
   background-color: #ebf0ff;
   border-radius: 15px;
   padding: 30px;
   border: 1px solid #ced4da;
 `;
+    
