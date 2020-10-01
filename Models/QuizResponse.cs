84823ea4c14f09b69b50d677ed188f59598d5f89
Joshua <joshua.sheng.fu@gmail.com>
2020-10-01 17:35:09 +1300

    Change naming convention

--- Models/QuizResponse.cs
+++ Models/QuizResponse.cs
@@ -1,15 +1,15 @@
 ﻿public class QuizResponse
 {
-    public string questionOne;
-    public string questionTwo;
-    public string questionThree;
-    public string questionFour;
-    public string questionFive;
-    public string questionSix;
-    public string questionSeven;
-    public string questionEight;
-    public string questionNine;
-    public string questionTen;
+    public string question1;
+    public string question2;
+    public string question3;
+    public string question4;
+    public string question5;
+    public string question6;
+    public string question7;
+    public string question8;
+    public string question9;
+    public string question10;
     public string age;
     public string gender;
     public string jaundice;
@@ -19,7 +19,7 @@
     public string Concatenate()
     {
         string response;
-        string[] list = new string[] { questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight, questionNine, questionTen };
+        string[] list = new string[] { question1, question2, question3, question4, question5, question6, question7, question8, question9, question10 };
 
         response = string.Join(",", list);
         response += "," + age + "," + gender + "," + jaundice + "," + familyASD;
