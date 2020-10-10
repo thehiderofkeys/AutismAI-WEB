56e25307888d04e47b3dc2a74abeff67f88db86e
Tyger Kong <tyger.kong@hotmail.com>
2020-10-10 16:37:43 +1300

    Fix index.js for quiz journey

--- Scripts/Home/react/Pages/QuizPage/index.js
+++ Scripts/Home/react/Pages/QuizPage/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import QuizPageContainer from "../../Container/QuizPageContainer";
 
 import QuizPage from "./QuizPage";
-import LastQuestion from "./LastQuestion"
+import LastQuestion from "./LastQuestion";
+import ResultsPage from "./ResultsPage";
+
 
 export default () => {
   return (
